510e4eba592a20faa8be5d14fd6e8e265d01b7c9
Alicia Weaver <aliciajweaver07@gmail.com>
2020-05-23 14:44:20 -0400

    Signup page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Signup from './signup';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Signup />
   </React.StrictMode>,
   document.getElementById('root')
 );
